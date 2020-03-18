8d9fc8a0fead21ba1602a5b661c99ee4002a550a
Sean Rooney <33811391+seanRoo@users.noreply.github.com>
2020-03-18 22:09:01 +0000

    conditional endpoint once more

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 import socketIOClient from 'socket.io-client';
 
 const App = () => {
-  //const endpoint = process.env.NODE_ENV==='development'?'http://localhost:8080/': '/';
-  const socket = socketIOClient();
+  const endpoint = process.env.NODE_ENV==='development'?'http://localhost:8080/': '/';
+  const socket = socketIOClient(endpoint);
 
   const containerStyle = {
     display: 'flex',
