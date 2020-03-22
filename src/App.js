d8d22a61ef68ce0c90e7392e4e606429b018695f
Sean Rooney <33811391+seanRoo@users.noreply.github.com>
2020-03-22 02:21:11 +0000

    fix build

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import "./App.css";
 import socketIOClient from "socket.io-client";
 
 const App = () => {
-  const endpoint =
-    process.env.NODE_ENV === "development" ? "http://localhost:8080/" : "/";
-  const socket = socketIOClient(endpoint);
+  // const endpoint =
+  //   process.env.NODE_ENV === "development" ? "http://localhost:8080/" : "/";
+  const socket = socketIOClient();
   const containerStyle = {
     display: "flex",
     justifyContent: "center",
