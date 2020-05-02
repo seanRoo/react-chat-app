9f5ccc689a301ae1e3a8d0ef99ce246dcc1e707f
Sean Rooney <33811391+seanRoo@users.noreply.github.com>
2020-05-02 12:25:06 +0100

    debugging the deploy

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import socketIOClient from "socket.io-client";
 
 const App = () => {
   console.log(process.env);
-  const endpoint = "http://localhost:8080/";
+  //const endpoint = "http://localhost:8080/";
   // process.env.NODE_ENV === "development" ? "http://localhost:8080/" : "/";
-  const socket = socketIOClient(endpoint);
+  const socket = socketIOClient();
   console.log(socket);
   const containerStyle = {
     display: "flex",
